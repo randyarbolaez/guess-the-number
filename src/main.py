828f0c205f0956a295bd6ac9423ddb54a55e8869
randyarbolaez <rarbolaez001@gmail.com>
2020-12-25 11:28:44 -0500

    implemented insertion sort

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from random import randint
 
-numbers_guessed = []
 number_to_guess = randint(0,50)
+
+def sorting_numbers_guessed(data):
+    for i in range(1,len(data)):
+
+        key = data[i]
+        j = i - 1
+
+        while(j >=0 and data[j] > key):
+            data[j+1] = data[j]
+            j -= 1
+        data[j+1] = key
+
+    return data
+
 
 def error_handling(prompt):
     while True:
@@ -12,9 +25,16 @@ def error_handling(prompt):
             print('That\'s not a number!')
 
 def guess_number():
+    numbers_guessed = []
     while True:
-        num = error_handling('What number do you think it is? ')
-        numbers_guessed.append(num)
+        num = error_handling('What number do you think it is?(0-50) ')
+        if num in numbers_guessed:
+            print('Already guessed that')
+            continue
+        else:
+            numbers_guessed.append(num)
+            sorted_numbers_guessed = sorting_numbers_guessed(numbers_guessed)
+            numbers_guessed = sorted_numbers_guessed
         if num == number_to_guess:
             print('Correct!!!!!!! :)' )
             return
